8c41dd85b4d06399eabe071aaa65bb472120f7d9
Olivier D <olivier.dauloudet@hotmail.com>
2021-04-30 00:02:52 +0200

    add some conda file

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open('README.rst') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    my_license = f.read()
 
 setup(
-    name='conda_template',
+    name='name of your project',
     version='0.1.0',
-    description='conda and pip project template',
+    description='put your description',
     long_description=readme,
     author='Dauloudet Olivier',
-    url='https://github.com/Smeaol22/conda_template',
-    license=license,
+    url='https://github.com/Smeaol22/****',
+    license=my_license,
     packages=find_packages(exclude=('tests', 'docs', 'example', 'conda'))
 )
