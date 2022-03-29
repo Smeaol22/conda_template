ae3aa65da1a17fabe864ca1715636872a8d00bc8
smeaol22 <olivier.dauloudet@hotmail.com>
2022-03-30 00:54:41 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -10,11 +10,12 @@ with open('LICENSE') as f:
 
 setup(
     name='name of your project',
-    version='0.1.0',
+    version='0.0.1',
     description='put your description',
     long_description=readme,
     author='Dauloudet Olivier',
     url='https://github.com/Smeaol22/****',
     license=my_license,
-    packages=find_packages(exclude=('tests', 'docs', 'example', 'conda'))
+    package_dir={'': 'src'},
+    packages=find_packages('src')
 )
